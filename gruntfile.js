081d08bacf256d83af66092c1ed70c010aa9b67b
Jessica Dembe <jmdembe@gmail.com>
2017-09-03 16:03:18 -0400

    configured jshint files with things to ignore, configured grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,23 @@
 module.exports = function(grunt) {
   "use strict";
-    // Do grunt-related things in here
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'), 
+      
+      clean: ['build/'];
+      
+      jshint: {
+        options: {
+          jshintrc: '.jshintrc', 
+          ignores: ['node_modules/**']
+        },
+        source: {
+          files: {
+            src: ['src/client/js/**/*.js']
+          }
+        }
+      }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
   };
