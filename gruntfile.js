075e8cbf55ac89cc702dba72bbc30eb904e0dd12
Jessica Dembe <jmdembe@gmail.com>
2017-09-03 17:43:08 -0400

    re-named module, edited index, added to gruntfile, installed angular locally, edited home template

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'), 
       
-      clean: ['build/'],
+      clean: ['dist/'],
 
       jshint: {
         options: {
@@ -15,14 +15,55 @@ module.exports = function(grunt) {
             src: ['src/client/js/**/*.js']
           }
         }
+      },
+      copy: {
+        html: {
+          files: [
+            {
+              expand: true, 
+              cwd: 'src/client',
+              src: '**/*.html',
+              dest: 'dist/'
+            }
+          ]
+        }, 
+        customjs: {
+          files: [
+            {
+              expand: true,
+              cwd: 'src/client/js',
+              src: '**/*.js',
+              dest: 'dist/js'
+            }
+          ]
+        },
+        // vendorjs: {
+        //   files: [
+        //     {
+        //       expand: true,
+        //       cwd: 'node_modules/angular',
+        //       src: ['angular.min.js'], 
+        //       dest: 'dist/js/vendor/'
+        //     }, 
+        //     {
+        //       expand: true,
+        //       cwd: 'node_modules/@uirouter/angularjs/release',
+        //       src: ['angular-ui-router.min.js'], 
+        //       dest: 'dist/js/vendor/'
+
+        //     }
+        //   ]
+        // }
+
       }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
 
     grunt.registerTask('sanity', ['clean']);
-    grunt.registerTask('default', ['clean', 'jshint', 'copy', 'concat']);
+    grunt.registerTask('default', ['clean', 'jshint', 'copy']);
   };
