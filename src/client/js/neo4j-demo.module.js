d8948da950e66158b0b88b41882df499404a2bb5
Jessica Dembe <jmdembe@gmail.com>
2017-09-04 12:23:42 -0400

    removed first rendition of index.html, commented out ui-router for debugging

--- src/client/js/neo4j-demo.module.js
+++ src/client/js/neo4j-demo.module.js
@@ -1,19 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module ('neo4j-demo')
-        .config(routerConfig);
+    angular.module ('neo4j-demo', []);
 
-    routerConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-    function routerConfig($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.when('', '/');
-
-        $stateProvider.state({
-            name: 'home',
-            url:'/',
-            templateUrl: 'views/home.template.html'
-        });
-
-    }
 }());
