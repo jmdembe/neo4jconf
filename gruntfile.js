cc1eaeafa2f641de4364e63a624bb40411319e87
Jessica Dembe <jmdembe@gmail.com>
2017-09-10 12:01:05 -0400

    Added folder for neo exports, intro and product vies, taks in gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -60,8 +60,10 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
 
     grunt.registerTask('sanity', ['clean']);
