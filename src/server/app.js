5509e6884a968facf4b082bfff0f0d07730c1692
Jessica Dembe <jmdembe@gmail.com>
2017-09-18 17:07:59 -0400

    Server works and is connected to Neo4j DB

--- src/server/app.js
+++ src/server/app.js
@@ -14,7 +14,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'neoadmin'));
+var session = driver.session();
+
 app.get('/', function(req, res) {
+    session
+        .run('MATCH(n:Person) RETURN n LIMIT 25')
+        .then(function(result) {
+            result.records.forEach(function(record) {
+                console.log(record._fields[0].properties);
+            });
+        })
+        .catch(function(err){
+            console.error(err);
+        });
     res.send('HALLO!');
 });
 
